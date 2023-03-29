00e747c790b13fdc9060faf8fa8e4dd905406240
Wawrzeń <Piotr.Wawrzen@opitz-consulting.com>
2023-03-29 11:20:48 +0200

    r create terra dir and add tf files

---
+++ terra/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  # shared_credentials_file = "C:\Users\pwa\.aws\credentials"
+  profile = "aws_terra_acg"
+}
