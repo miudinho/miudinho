13d6d9cc10e5aaeaebf0801fe492b43dcbb039ea
Wawrzeń <Piotr.Wawrzen@opitz-consulting.com>
2023-03-30 11:07:28 +0200

    update r terra dir with new tf files

---
+++ terra/output.tf
@@ -0,0 +1,12 @@
+output "dev_node_instance_type" {
+    value = aws_instance.dev_node.instance_type
+}
+
+output "dev_node_instance_public_ip" {
+    value = aws_instance.dev_node.public_ip
+}
+
+output "dev_node_instance_root_block_device" {
+    value = aws_instance.dev_node.root_block_device[*]
+}
+
