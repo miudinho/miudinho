7dd5bebe18dcd4a310917e45b627cb06fc7495d9
Wawrzeń <Piotr.Wawrzen@opitz-consulting.com>
2023-03-31 14:00:07 +0200

    r datasource modification

--- terra/datasources.tf
+++ terra/datasources.tf
@@ -1,3 +1,4 @@
+#resources 
 data "aws_ami" "server_ami" {
     most_recent = true
     #plural - argument list 
@@ -7,4 +8,9 @@ data "aws_ami" "server_ami" {
         name = "name"
         values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
     }
+}
+
+data "http" "www_ip" {
+    url = "http://target.me"
+    # data.http.www_ip.body
 }
