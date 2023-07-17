c2e64bc253a98f6a86b14f3e1b4a92a807129ff3
Wawrzeń <Piotr.Wawrzen@opitz-consulting.com>
2023-07-17 08:26:42 +0200

    remove providers keys

--- terra/providers.tf
+++ terra/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQGCV7V5G4B5NSRCT"
-  secret_key = "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"
+  access_key = "xxxx"
+  secret_key = "yyyyyyyyyyy"
 }
