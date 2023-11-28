e95f9ec5f01a8d1528337d2608a4c1eac44dfe8e
Wawrzeń <Piotr.Wawrzen@opitz-consulting.com>
2023-11-28 09:45:11 +0100

    LernGruppe

---
+++ LernGruppe/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]],["vagrant-vboxmanage",["= 0.0.2"]]],"checksum":"2b5bbc6a74b03deda0503716beade930819f4e8928ebae78f7ab65f053ad2b1b","vagrant_version":"2.3.3"}
